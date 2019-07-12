a75e6d24c7c8fc90ded306ff07ac2d0601d2137a
Michael Nutt <mnutt346@gmail.com>
2019-07-12 17:40:59 -0500

    Inject message service into hero service and display the message from hero service.

--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -1,9 +1,17 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { timingSafeEqual } from "crypto";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class MessageService {
+  messages: string[] = [];
 
-  constructor() { }
+  add(message: string) {
+    this.messages.push(message);
+  }
+
+  clear() {
+    this.messages = [];
+  }
 }
